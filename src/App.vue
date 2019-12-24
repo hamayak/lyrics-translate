8fac02384e3386f1dc53c3f861f5c434df51540c
mastercss <davit.sahakyan3.y@tumo.org>
2019-12-24 19:33:57 +0400

    app bar sarkeci

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,118 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app >
+    <v-app-bar app color="#242424" dark>
+      <v-app-bar-nav-icon class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title @click="$router.push('/')">Lyrics App</v-toolbar-title>
+      <!-- <v-img src="./assets/flooop.png" /> -->
       <v-spacer></v-spacer>
+      <v-text-field append-outer-icon="mdi-magnify"
+            v-model="searchText"
+            solo clearable hide-details single-line
+            placeholder="Search"></v-text-field>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Artist</v-btn>
+        <v-btn text>Songs</v-btn>
+        <v-btn text>Colections</v-btn>
+        <v-menu
+      origin="center center"
+      transition="scale-transition"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="#242424"
+          v-on="on"
+        >
+        Languages
+        </v-btn>
+      </template>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+      </v-toolbar-items>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+
+     <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Artist</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-music-note</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Songs</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon> mdi-library-music</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Colections</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+
+</v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    data: () => ({
+      items: [
+        { title: 'English' },
+        { title: 'Russian' },
+        { title: 'Armenian' },
+        { title: 'French' },
+      ]
+    }),
+  }
 </script>
